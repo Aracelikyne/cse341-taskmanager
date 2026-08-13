0999a46e8adc64386e3636e0b7fb67ca6aef0c6d
aracelikyne <aracelikyne@gmail.com>
2026-08-12 18:39:02 -0600

    fixed file validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,87 +1,50 @@
-const validator = require('../helpers/validate');
+const { body, validationResult } = require('express-validator');
 
-const saveTask = (req, res, next) => {
-  const validationRule = {
-    title: 'required|string',
-    description: 'required|string',
-    dueDate: 'required|string',
-    status: 'required|string',
-    priority: 'required|string',
-    projectId: 'required|string',
-    assignedUserId: 'required|string'
-  };
-  validator(req.body, validationRule, {}, (err, status) => {
-    if (!status) {
-      res.status(400).send({
-        success: false,
-        message: 'Validation failed',
-        data: err
-      });
-    } else {
-      next();
-    }
+// This handles throwing the 400 error if any rules fail
+const validator = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ 
+    success: false, 
+    message: 'Validation failed', 
+    errors: errors.array() 
   });
 };
 
-const saveCategory = (req, res, next) => {
-  const validationRule = {
-    name: 'required|string',
-    color: 'required|string'
-  };
-  validator(req.body, validationRule, {}, (err, status) => {
-    if (!status) {
-      res.status(400).send({
-        success: false,
-        message: 'Validation failed',
-        data: err
-      });
-    } else {
-      next();
-    }
-  });
-};
+const saveTask = [
+  body('title').notEmpty().isString().withMessage('Title is required'),
+  body('description').notEmpty().isString().withMessage('Description is required'),
+  body('dueDate').notEmpty().isString().withMessage('Due date is required'),
+  body('status').notEmpty().isString().withMessage('Status is required'),
+  body('priority').notEmpty().isString().withMessage('Priority is required'),
+  body('projectId').notEmpty().isString().withMessage('Project ID is required'),
+  body('assignedUserId').notEmpty().isString().withMessage('Assigned User ID is required'),
+  validator
+];
 
-// NEW: Validation for Users
-const saveUser = (req, res, next) => {
-  const validationRule = {
-    username: 'required|string',
-    email: 'required|email',
-    firstName: 'required|string',
-    lastName: 'required|string'
-  };
-  validator(req.body, validationRule, {}, (err, status) => {
-    if (!status) {
-      res.status(400).send({
-        success: false,
-        message: 'Validation failed',
-        data: err
-      });
-    } else {
-      next();
-    }
-  });
-};
+const saveCategory = [
+  body('name').notEmpty().isString().withMessage('Name is required'),
+  body('color').notEmpty().isString().withMessage('Color is required'),
+  validator
+];
 
-// NEW: Validation for Projects
-const saveProject = (req, res, next) => {
-  const validationRule = {
-    title: 'required|string',
-    description: 'required|string',
-    ownerId: 'required|string',
-    startDate: 'required|string'
-  };
-  validator(req.body, validationRule, {}, (err, status) => {
-    if (!status) {
-      res.status(400).send({
-        success: false,
-        message: 'Validation failed',
-        data: err
-      });
-    } else {
-      next();
-    }
-  });
-};
+const saveUser = [
+  body('username').notEmpty().isString().withMessage('Username is required'),
+  body('email').isEmail().withMessage('Valid email is required'),
+  body('firstName').notEmpty().isString().withMessage('First name is required'),
+  body('lastName').notEmpty().isString().withMessage('Last name is required'),
+  validator
+];
+
+const saveProject = [
+  body('title').notEmpty().isString().withMessage('Title is required'),
+  body('description').notEmpty().isString().withMessage('Description is required'),
+  body('ownerId').notEmpty().isString().withMessage('Owner ID is required'),
+  body('startDate').notEmpty().isString().withMessage('Start date is required'),
+  validator
+];
 
 module.exports = {
   saveTask,
